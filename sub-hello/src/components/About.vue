f586168f33ccca183f2bd86d223f51622325eb56
郭辉 <guohui@iautos.cn>
2021-03-10 17:28:39 +0800

    [update]

---
+++ sub-hello/src/components/About.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    about asdasdasdasd
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ sub-hello/src/components/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    home 
+    <router-link to="/about">about</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
